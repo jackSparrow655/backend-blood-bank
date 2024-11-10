8a8419e2b1d1b6ba20870219480d64d2508f6ede
Arijit655 <arijitbarik655@gmail.com>
2024-11-10 14:10:57 +0530

    change cors

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     origin:['http://localhost:3000',
-        'https://blood-bank-frontend-umg6.onrender.com',
+        'https://client-pied-zeta.vercel.app',
     ],
     methods:['GET', 'POST','PUT','DELETE'],
     credentials:true
