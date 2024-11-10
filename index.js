6226533a24c0be715bf62ba86dda4d2811d9970f
Arijit655 <arijitbarik655@gmail.com>
2024-11-10 14:27:44 +0530

    update cors

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     origin:['http://localhost:3000',
-        'https://client-pied-zeta.vercel.app',
+        'https://client-ecru-three.vercel.app',
         '*'
     ],
     methods:['GET', 'POST','PUT','DELETE'],
