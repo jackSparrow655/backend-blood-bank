ddb6ed5157fe0a8a4d1107538262ba1725a3b965
Arijit655 <arijitbarik655@gmail.com>
2025-01-25 15:16:23 +0530

    changes

--- config/db.js
+++ config/db.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     console.log(process.env.MONGO_URL)
-    await mongoose.connect(process.env.MONGO_URL,{
-        useNewUrlParser:true,
-        useUnifiedTopology:true
-    }).then(() => {
+    await mongoose.connect(process.env.MONGO_URL).then(() => {
         console.log("DB connection is successfull")
     }).catch((err) => {
         console.log("error in db connection", err.message) 
--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     origin:['http://localhost:3000',
+            'http://localhost:3001',
         'https://client-ecru-three.vercel.app',
         '*'
     ],
